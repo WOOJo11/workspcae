f4ec0be36bcd0de15cac8a5f9d1945e4a78e4ff8
DESKTOP-IJ8C807\user1 <anya92@naver.com>
2023-02-16 17:28:08 +0900

    16/02/23 수업 내용

---
+++ 1_Java/a005_oop/src/edu/kh/oop/method/dto/UserRun.java
@@ -0,0 +1,14 @@
+package edu.kh.oop.method.dto;
+
+public class UserRun {
+
+	public static void main(String[] args) {
+		UserView view= new UserView();
+		
+		view.displayMenu();
+		
+		
+		
+		
+	}
+}
