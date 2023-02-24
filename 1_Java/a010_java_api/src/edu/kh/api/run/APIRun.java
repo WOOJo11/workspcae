35ed54db20d2c3c48bed2d764928ccb4c59f85dd
DESKTOP-IJ8C807\user1 <anya92@naver.com>
2023-02-24 17:04:26 +0900

    24/02/23 수업내용

---
+++ 1_Java/a010_java_api/src/edu/kh/api/run/APIRun.java
@@ -0,0 +1,21 @@
+package edu.kh.api.run;
+
+import edu.kh.api.dto.APIView;
+
+public class APIRun {
+
+	
+	public static void main(String[] args) {
+		
+		APIView view = new APIView();
+		
+		view.displayMenu();
+		
+		
+	}
+		 
+		 
+		 
+	 
+	 
+}
